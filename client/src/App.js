b2c21cd499fe53dd4de486a11d067cf4b8d2c7cf
Ritesh Jangir <79959578+mrriteshjangir@users.noreply.github.com>
2021-08-29 16:15:00 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Signin from "./views/Signin";
 import Signup from "./views/Signup";
+import Dashbaord from "./views/Dashbaord";
 
 import useToken from "./components/useToken";
 
@@ -11,15 +12,30 @@ function App() {
   const { token, setToken } = useToken();
 
   if(!token) {
-    return <Signin setToken={setToken} />
+    return (
+      <Router>
+        <Switch>
+          
+          <Route exact path="/sign-up" component={Signup} />
+
+          <Route path="/">
+            <Signin setToken={setToken} />
+          </Route>
+
+        </Switch>
+      </Router>
+      );
   }
   else
   {
     return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Signin} />
-        <Route exact path="/sign-up" component={Signup} />
+        <Route exact path="/">
+        {token? <Redirect to="/dashbaord"/> : <Signin />}
+        </Route>
+        <Route path="/sign-up" component={Signup} />
+        <Route path="/dashbaord" component={Dashbaord} />
       </Switch>
     </Router>
     );
